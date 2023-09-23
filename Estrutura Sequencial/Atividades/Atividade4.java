302d05834e2984d84edef85e5944aff2e404a3f3
Giovanna Pergentino <giovannapergentinocs@gmail.com>
2023-09-23 17:56:54 -0300

    Create Atividade4.java

---
+++ Estrutura Sequencial/Atividades/Atividade4.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Atividade4 {
+
+	public static void main(String[] args) {
+		//Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
+		//hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas
+		//decimais
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int numFuncionario = sc.nextInt();
+		int horasTrabalhadas = sc.nextInt();
+		double ValorPorHoras = sc.nextDouble();	
+		
+		double salario = horasTrabalhadas * ValorPorHoras;
+		
+		System.out.println("NUMBER = " + numFuncionario);
+		System.out.printf("SALARY = U$ %.2f%n", salario);
+		sc.close();
+		
+	}
+
+}
